e5f667f04722d762aeb52150bbf30a1b98ead28c
filipko_smileface <43612452+filipkosmileface@users.noreply.github.com>
2022-05-17 22:07:10 +0200

    Upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const RPC = require("discord-rpc");
+const client = new RPC.Client({transport: "ipc"});
+
+const activity = {
+    details: "Čas strávený v Matrixe:",
+    assets: {
+        large_image: "image_3",
+        large_text: "Skús ma odpojiť a zabijem ťa.",
+        small_image: "image_3",
+        small_text: "Sleep..."
+    },
+    buttons: [
+        {
+            label: "Check Matrix",
+            url: "https://www.matrixresurrections.net/"
+        }
+    ],
+    timestamps: {start: Date.now()},
+    instance: true
+};
+
+client.on("ready", () => {
+    client.request("SET_ACTIVITY", {pid: process.pid, activity: activity});
+    console.log("done");
+})
+
+client.login({clientId: "clientid vašej aplikácie"});
+// https://discord.com/developers/applications
